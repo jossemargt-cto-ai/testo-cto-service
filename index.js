b26d7423f099294516d9d5d868b77c53d0ed5ff8
Jossemar Cordero <106267440+jossemargt-cto-ai@users.noreply.github.com>
2022-08-08 11:43:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http')
 async function main() {
   const requestListener = function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.write('1658985161210')
+    res.write('lun 08 ago 2022 11:43:08 CST')
     res.end()
   }
 
